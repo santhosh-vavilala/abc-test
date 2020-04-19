005a22e681d5cbb195ecd7deeafb79a3ba471253
Kumar Vavilala, S <s.kumar.vavilala@accenture.com>
2020-04-19 13:00:01 +0530

    Implement state

--- App.js
+++ App.js
@@ -1,55 +1,104 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, Button, Alert  } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground, Button, Alert } from 'react-native';
+import * as a from './assets/a.png'
+
 
 
 const image = { uri: "https://reactjs.org/logo-og.png" };
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.container}>
 
-        <ImageBackground source={require('./assets/d.png')} style={styles.image}>
-          <Text style={styles.text}>B for Banana.</Text>
+export default class App extends Component{  
 
+  constructor(){
+ 
+    super();
+ 
+    this.state={
+ 
+      currentIndex : 0,
+      images: [
+        {
+          id: 0,
+          path: require('./assets/a.png'),
+          text: 'A for Apple'
+        },
+        {
+          id: 1,
+          path: require('./assets/b.png'),
+          text: 'B for Banana'
+        },
+        {
+          id: 2,
+          path: require('./assets/c.png'),
+          text: 'C for Cat'
+        },
+        {
+          id: 0,
+          path: require('./assets/o.jpg'),
+          text: 'O for Orange'
+        }
+      ]
+ 
+    }
+ 
+  }
+  handleNext=()=>{
+ 
+    this.setState({
+ 
+      currentIndex : (this.state.currentIndex < this.state.images.length - 1) ?  this.state.currentIndex + 1 : 0
+ 
+    })
+  }
+ 
+  render() {
+ 
 
-        </ImageBackground>
-        {/* <Text>yyy up App.js to start working on your app!</Text> */}
-
-
-
+    return (
+      <View style={styles.container}> 
+        <View style={styles.container}>
+  
+          <ImageBackground source={this.state.images[this.state.currentIndex].path} style={styles.image}>
+            <Text style={styles.text}>{this.state.images[this.state.currentIndex].text}</Text>
+  
+  
+          </ImageBackground>
+          {/* <Text>yyy up App.js to start working on your app!</Text> */}
+  
+  
+  
+        </View>
+        <View style={{ flex: 1, flexDirection: 'row', top: 300, justifyContent: "center" }}>
+          <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white', height: 100 }}>
+            <Button
+              title="Previous"
+              color="powderblue"
+              onPress={() => Alert.alert('Button with adjusted color pressed')}
+            />
+          </View> 
+  
+          <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white' }}>
+            <Button
+              title="Home"
+              color="skyblue"
+  
+              onPress={() => Alert.alert('Button with adjusted color pressed')}
+            />
+          </View>
+  
+  
+          <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white' }}>
+            <Button
+              title="Next"
+              color="steelblue"
+              onPress={this.handleNext}
+            />
+          </View>
+  
+        </View>
       </View>
-      <View style={{ flex: 1, flexDirection: 'row', top: 300, justifyContent:"center" }}>
-        <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white', height: 100 }}>
-        <Button 
-          title="Previous"
-          color="powderblue" 
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-        </View>
-
-        <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white' }}>
-        <Button 
-          title="Home"
-          color="skyblue" 
-
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-        </View>
-
-
-        <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'white' }}>
-        <Button 
-          title="Next"
-          color="steelblue" 
-
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-        </View>
-        
-      </View>
-    </View>
-  );
-}
+    );
+  }
+ }
 
 const styles = StyleSheet.create({
   container: {
